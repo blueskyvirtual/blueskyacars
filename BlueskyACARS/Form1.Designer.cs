6b47c36bd5ec85170267eb46d34fd39a765929b1
Matthew J. Kosmoski <mkosmo@gmail.com>
2018-05-05 23:48:04 -0500

    Travis CI (#3)

--- BlueskyACARS/Form1.Designer.cs
+++ BlueskyACARS/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace BlueskyACARS
+﻿namespace BlueskyAcars
 {
     partial class Form1
     {
@@ -28,13 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLatitude = new System.Windows.Forms.TextBox();
+            this.txtLongitude = new System.Windows.Forms.TextBox();
+            this.txtAltitude = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.statusBar = new System.Windows.Forms.StatusStrip();
+            this.statusBarConnected = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusBarErrors = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Latitude";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Longitude";
+            // 
+            // txtLatitude
+            // 
+            this.txtLatitude.Location = new System.Drawing.Point(72, 49);
+            this.txtLatitude.Name = "txtLatitude";
+            this.txtLatitude.ReadOnly = true;
+            this.txtLatitude.Size = new System.Drawing.Size(189, 20);
+            this.txtLatitude.TabIndex = 3;
+            // 
+            // txtLongitude
+            // 
+            this.txtLongitude.Location = new System.Drawing.Point(72, 75);
+            this.txtLongitude.Name = "txtLongitude";
+            this.txtLongitude.ReadOnly = true;
+            this.txtLongitude.Size = new System.Drawing.Size(189, 20);
+            this.txtLongitude.TabIndex = 4;
+            // 
+            // txtAltitude
+            // 
+            this.txtAltitude.Location = new System.Drawing.Point(72, 101);
+            this.txtAltitude.Name = "txtAltitude";
+            this.txtAltitude.ReadOnly = true;
+            this.txtAltitude.Size = new System.Drawing.Size(189, 20);
+            this.txtAltitude.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Altitude";
+            // 
+            // statusBar
+            // 
+            this.statusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusBarConnected,
+            this.statusBarErrors});
+            this.statusBar.Location = new System.Drawing.Point(0, 129);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(284, 22);
+            this.statusBar.TabIndex = 8;
+            this.statusBar.Text = "statusStrip1";
+            // 
+            // statusBarConnected
+            // 
+            this.statusBarConnected.Name = "statusBarConnected";
+            this.statusBarConnected.Size = new System.Drawing.Size(79, 17);
+            this.statusBarConnected.Text = "Disconnected";
+            // 
+            // statusBarErrors
+            // 
+            this.statusBarErrors.Name = "statusBarErrors";
+            this.statusBarErrors.Size = new System.Drawing.Size(37, 17);
+            this.statusBarErrors.Text = "Errors";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 151);
+            this.Controls.Add(this.statusBar);
+            this.Controls.Add(this.txtAltitude);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLongitude);
+            this.Controls.Add(this.txtLatitude);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "BlueskyACARS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusBar.ResumeLayout(false);
+            this.statusBar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLatitude;
+        private System.Windows.Forms.TextBox txtLongitude;
+        private System.Windows.Forms.TextBox txtAltitude;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.StatusStrip statusBar;
+        private System.Windows.Forms.ToolStripStatusLabel statusBarConnected;
+        private System.Windows.Forms.ToolStripStatusLabel statusBarErrors;
     }
 }
 
--- BlueskyACARS/Form1.cs
+++ BlueskyACARS/Form1.cs
@@ -6,15 +6,164 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 
-namespace BlueskyACARS
+using LockheedMartin.Prepar3D.SimConnect;
+using System.Runtime.InteropServices;
+
+namespace BlueskyAcars
 {
     public partial class Form1 : Form
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        const int WM_USER_SIMCONNECT = 0x0402;
+        SimConnect simconnect = null;
+
+        enum DEFINITIONS
+        {
+            Struct1,
+        }
+
+        enum DATA_REQUESTS
+        {
+            REQUEST_1,
+        };
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+        struct Struct1
+        {
+            // this is how you declare a fixed size string
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+            public String title;
+            public double latitude;
+            public double longitude;
+            public double altitude;
+            public double altitude_agl;
+            public double weight_on_wheels;
+            public double vertical_speed;
+        };
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        protected override void DefWndProc(ref Message m)
+        {
+            if (m.Msg == WM_USER_SIMCONNECT)
+            {
+                if (simconnect != null)
+                {
+                    simconnect.ReceiveMessage();
+                }
+            }
+            else
+            {
+                base.DefWndProc(ref m);
+            }
+        }
+
+        private void simconnect_Open()
+        {
+            try
+            {
+                simconnect.OnRecvOpen += new SimConnect.RecvOpenEventHandler(simconnect_OnRecvOpen);
+                simconnect.OnRecvQuit += new SimConnect.RecvQuitEventHandler(simconnect_OnRecvQuit);
+                simconnect.OnRecvException += new SimConnect.RecvExceptionEventHandler(simconnect_OnRecvException);
+
+                simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "Title", null, SIMCONNECT_DATATYPE.STRING256, 0.0f, SimConnect.SIMCONNECT_UNUSED); // aircraft title
+                simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "Plane Latitude", "degrees", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED); // latitude
+                simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "Plane Longitude", "degrees", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED); // longitude
+                simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "Plane Altitude", "feet", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED); // altitude
+                simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "PLANE ALT ABOVE GROUND", "feet", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED); // altitude_agl
+                simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "SIM ON GROUND", "Bool", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED); // weight_on_wheels
+                simconnect.AddToDataDefinition(DEFINITIONS.Struct1, "VELOCITY WORLD Y", "feet per second", SIMCONNECT_DATATYPE.FLOAT64, 0.0f, SimConnect.SIMCONNECT_UNUSED); // vertical_speed
+
+                simconnect.RegisterDataDefineStruct<Struct1>(DEFINITIONS.Struct1);
+                simconnect.OnRecvSimobjectDataBytype += new SimConnect.RecvSimobjectDataBytypeEventHandler(simconnect_OnRecvSimobjectDataBytype);
+            }
+            catch (COMException e)
+            {
+                throw e;
+            }
+        }
+
+        private void simconnect_Close()
+        {
+            if (simconnect != null)
+            {
+                simconnect.Dispose();
+                simconnect = null;
+            }
+        }
+
+        void simconnect_OnRecvOpen(SimConnect sender, SIMCONNECT_RECV_OPEN data)
+        {
+
+        }
+
+        void simconnect_OnRecvQuit(SimConnect sender, SIMCONNECT_RECV data)
+        {
+            simconnect_Close();
+        }
+
+        void simconnect_OnRecvException(SimConnect sender, SIMCONNECT_RECV_EXCEPTION data)
+        {
+
+        }
+
+        void simconnect_OnRecvSimobjectDataBytype(SimConnect sender, SIMCONNECT_RECV_SIMOBJECT_DATA_BYTYPE data)
+        {
+
+            switch ((DATA_REQUESTS)data.dwRequestID)
+            {
+                case DATA_REQUESTS.REQUEST_1:
+                    Struct1 s1 = (Struct1)data.dwData[0];
+
+                    log.Debug("Latitude: " + s1.latitude);
+                    log.Debug("Longitude: " + s1.longitude);
+                    log.Debug("Altitude MSL: " + s1.altitude);
+                    log.Debug("Altitude AGL: " + s1.altitude_agl);
+                    log.Debug("Weight on Wheels: " + s1.weight_on_wheels);
+                    log.Debug("Vertical Velocity (fps): " + s1.vertical_speed);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (simconnect == null)
+            {
+                try
+                {
+                    simconnect = new SimConnect("BlueskyACARS", this.Handle, WM_USER_SIMCONNECT, null, 0);
+                    simconnect_Open();
+                    statusBarConnected.Text = "Connected";
+                    System.Timers.Timer timer = new System.Timers.Timer(1000);
+                    timer.Elapsed += runSimConnectHandler;
+                    timer.AutoReset = true;
+                    timer.Enabled = true;
+                    timer.Start();
+                }
+                catch (COMException ex)
+                {
+                    statusBarErrors.Text = "Unable to Connect - " + ex.Message;
+                }
+            }
+        }
+
+        private void runSimConnectHandler(Object source, ElapsedEventArgs e)
+        {
+            simconnect.RequestDataOnSimObjectType(DATA_REQUESTS.REQUEST_1, DEFINITIONS.Struct1, 0, SIMCONNECT_SIMOBJECT_TYPE.USER);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            statusBarErrors.Text = "";
         }
     }
 }
